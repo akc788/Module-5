21be20bdfcd18e532832ab15ec70cd8717947184
Aakash kc <117092331+akc788@users.noreply.github.com>
2026-02-24 14:35:47 +0200

    Add files via upload

---
+++ Optional_exc.py
@@ -0,0 +1,105 @@
+class Node():
+    def __init__(self, data, parent_node=None, left_child=None, right_child=None):
+        self.data = data
+        self._parent = parent_node
+        self._left_child = left_child
+        self._right_child = right_child
+
+    def __repr__(self):
+        left = self._left_child if self._left_child is not None else ''
+        right = self._right_child if self._right_child is not None else ''
+        return f'{self.data}<{left}><{right}>#'
+
+class Tree():
+    def __init__(self):
+        self._root_node = None
+
+    def __repr__(self):
+        return f'<Tree: {self._root_node}>'
+
+    def insert(self, data):
+        current_node = self._root_node
+        parent_node = None
+        while current_node:
+            parent_node = current_node
+            if data <= current_node.data:
+                current_node = current_node._left_child
+            else:
+                current_node = current_node._right_child
+        new_node = Node(data, parent_node=parent_node)
+        if parent_node is None:
+            if self._root_node is None:
+                self._root_node = new_node
+            else:
+                raise(ValueError)
+        elif new_node.data < parent_node.data:
+            parent_node._left_child = new_node
+        else:
+            parent_node._right_child = new_node
+
+    def _find(self, data):
+        current = self._root_node
+        while current:
+            if current.data == data:
+                return current
+            elif current.data > data:
+                current = current._left_child
+            else:
+                current = current._right_child
+        return None        
+
+    def delete_node(self, data):
+        node = self._find(data)
+        if node is None:
+            return  # Node not found
+
+        
+        if node._left_child is None and node._right_child is None:
+            if node._parent:
+                if node._parent._left_child == node:
+                    node._parent._left_child = None
+                else:
+                    node._parent._right_child = None
+            else:
+                self._root_node = None
+            node._parent = None
+
+        elif node._left_child is None or node._right_child is None:
+            child = node._left_child if node._left_child else node._right_child
+            if node._parent:
+                if node._parent._left_child == node:
+                    node._parent._left_child = child
+                else:
+                    node._parent._right_child = child
+                child._parent = node._parent
+            else:
+                self._root_node = child
+                child._parent = None
+            node._parent = None
+            node._left_child = None
+            node._right_child = None
+
+
+        else:
+            successor = node._right_child
+            while successor._left_child:
+                successor = successor._left_child
+
+            node.data = successor.data
+
+            if successor._parent._left_child == successor:
+                successor_parent = successor._parent
+                successor_child = successor._right_child
+                successor_parent._left_child = successor_child
+                if successor_child:
+                    successor_child._parent = successor_parent
+            else:
+                successor_parent = successor._parent
+                successor_child = successor._right_child
+                successor_parent._right_child = successor_child
+                if successor_child:
+                    successor_child._parent = successor_parent
+
+            successor._parent = None
+            successor._left_child = None
+            successor._right_child = None
